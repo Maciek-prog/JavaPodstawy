29c1cc67b0a4a58dcfe48a50eb6ccbce571d031f
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-18 13:45:31 +0200

    zad8

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj pierwszą liczbę: ");
+        float number = scanner.nextFloat();
+        System.out.println("Wybierz znak: ");
+        String sign = scanner.next();
+        System.out.println("Podaj drugą liczbę: ");
+        float number1 = scanner.nextFloat();
 
     }
 }
