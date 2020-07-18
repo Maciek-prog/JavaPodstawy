47e5ce99d62e133d99fd5a3316d245da2ca8e26c
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-18 11:39:15 +0200

    zad3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+        double delta = (b*b) - 4*a*c;
+        if(delta>0){
+            double x1 = -b - Math.sqrt(delta)/2*a;
+            System.out.println(x1);
+            double x2 = -b + Math.sqrt(delta)/2*a;
+            System.out.println(x2);
+        } else if(delta<0){
+            System.out.println("Delta ujemna");
+        }
     }
 }
