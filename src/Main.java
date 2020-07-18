6dd06e0932303ed31117a41bf6a926ec52fcb104
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-18 12:31:04 +0200

    zad4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbe: ");
+        int liczba = scanner.nextInt();
+        for (int a = 1; a <= liczba; a++) {
+
+            if ((a % 3 == 0) && (liczba % 7 == 0)) {
+
+                System.out.println("Pif Paf");
+            } else if (a % 3 == 0) {
+
+                System.out.println("Pif");
+
+            } else if (a % 7 == 0) {
+
+                System.out.println("Paf");
+
+            } else {
+
+                System.out.println(a);
+            }
+        }
 
 
     }
