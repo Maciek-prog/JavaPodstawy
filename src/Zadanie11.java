9d8c4a9880bf5175d59ee9f9fe37142a947fe4cd
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-19 11:02:08 +0200

    Zad11

--- src/Zadanie11.java
+++ src/Zadanie11.java
@@ -1,7 +1,21 @@
 import java.util.Scanner;
 
 public class Zadanie11 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String napis = scanner.next();
+        int dlugosc = 0;
+        String napis2 = "Nie podano tekstu :D";
+
+        while (!napis.equals("Starczy")) {
+
+            if (dlugosc < napis.length()) {
+                dlugosc = napis.length();
+                napis2 = napis;
+
+            }
+            napis= scanner.next();
+        }
+        System.out.println(napis2);
     }
 }
