de7108b9f07510c3657bab46f714b31b45616aaa
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-19 09:12:11 +0200

    Zad10

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+//        Napisz  program,  który  pobierze  od  użytkownika  jedną  liczbę  dodatnią  (typu int)  i obliczy  sumę  cyfr  podanej  liczby.
+//        Podpowiedź:  aby  rozpatrywać  liczbę  cyfra  po cyfrze,  możesz  obliczać  resztę  z  dzielenia  liczby  przez  10  (aby  uzyskać  wartość ostatniej cyfry) i dzielić liczbę bez reszty przez 10 (żeby „przesuwać” się do kolejnej cyfry)
+//        ------------------------------------------------
         Scanner scanner = new Scanner(System.in);
+        int number = scanner.nextInt();
+
 
     }
 }
