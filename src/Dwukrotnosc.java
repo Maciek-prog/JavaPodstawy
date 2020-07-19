d426a0c827e083d709aa1b0a336d1b624c4f4035
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-19 13:34:42 +0200

    Zad13

--- src/Dwukrotnosc.java
+++ src/Dwukrotnosc.java
@@ -1,3 +1,22 @@
-public class Dwukrotnosc {
+import java.util.Scanner;
 
+public class Dwukrotnosc {
+    //    Napisz  program,  który  pobierze  od  użytkownika  10  dowolnie  dużych  liczb (zmiennych typu int) i wypisze te
+//    ,które wystąpiły minimum dwukrotnie.
+    Scanner scanner = new Scanner(System.in);
+    private int value = scanner.nextInt();
+    private int iloscWystapien;
+
+
+    public int getValue() {
+        return value;
+    }
+
+    public int getiloscWystapien() {
+        return iloscWystapien;
+    }
+
+    public void setIloscWystapien(int iloscWystapien) {
+        this.iloscWystapien = iloscWystapien;
+    }
 }
---
+++ src/Zadanie15.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Zadanie15 {
+    private Scanner scanner = new Scanner(System.in);
+
+    public Dwukrotnosc[] getDwukrotsonsc() {
+        Dwukrotnosc[] dwukrotnosc2 = new Dwukrotnosc[10];
+
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Podaj liczbę: ");
+            int value = scanner.nextInt();
+
+            boolean found= false;
+
+            for (Dwukrotnosc dwukrotnosc : dwukrotnosc2) {
+
+                if (dwukrotnosc.getValue() == value) {
+                    dwukrotnosc.setIloscWystapien(dwukrotnosc.getiloscWystapien());
+                    found=true;
+
+                }
+            }
+            if(!found){
+                dwukrotnosc2[i] = new Dwukrotnosc(value, 1);
+            }
+        }
+        return dwukrotnosc2;
+    }
+    public void printNumber(){
+        for (Dwukrotnosc dwukrotnosc: dwukrotnosc2)
+
+    }
+}
+
