ef6c9fe9ccef4438cfe3db9c2d8459fd2d15c73b
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-19 10:16:19 +0200

    Zad10

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,16 @@ public class Main {
 //        Podpowiedź:  aby  rozpatrywać  liczbę  cyfra  po cyfrze,  możesz  obliczać  resztę  z  dzielenia  liczby  przez  10  (aby  uzyskać  wartość ostatniej cyfry) i dzielić liczbę bez reszty przez 10 (żeby „przesuwać” się do kolejnej cyfry)
 //        ------------------------------------------------
         Scanner scanner = new Scanner(System.in);
+
         int number = scanner.nextInt();
+        int signno = String.valueOf(number).length();
+        int sum = 0;
+        System.out.println(signno);
 
-
+        for (int i = 1; i <= signno; i++) {
+            sum += number % 10;
+            number = number / j;
+        }
+        System.out.println(sum);
     }
 }
