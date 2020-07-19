09eeef2adf4f1318b9d8885bcf360b78d9101d99
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-19 12:49:08 +0200

    Zad13

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,23 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         //ZADANIE 13
+
         //Jakanie jakanie = new Jakanie();
 //        jakanie.run();
 //   -----------------------------------------------
+
+
         //ZADANIE 14
+
         PobieranieLiczb pobieranieLiczb = new PobieranieLiczb();
         PobieranieLiczb.Chars aChar = pobieranieLiczb.getChar();
         System.out.println(pobieranieLiczb.getResult(aChar));
+
+//--------------------------------------------------------------------
+
+        //ZADANIE 15
 
     }
 }
