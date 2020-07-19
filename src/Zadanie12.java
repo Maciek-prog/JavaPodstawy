03d92eee09d0d1d1f4f8bb18b96d3ed2b14da8ba
Maciek-prog <pretkimaciej1@gmail.com>
2020-07-19 11:09:31 +0200

    Zad12

---
+++ src/Zadanie12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+//Napisz program, który pobierze od użytkownika tekst (zmienną typu String) i policzy jakim procentem wszystkich znaków tekstu był znak spacji,
+//liczba spacji/liczba wszystkich znaków wszystko razy 100%
+public class Zadanie12 {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        String word = scanner.next();
+
+        String znakSpcaji=" ";
+
+
+    }
+}
